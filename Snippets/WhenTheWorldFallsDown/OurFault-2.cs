95809e523738d35e2604a4a1cad3e8aee6dcb7ee
Joel Hammond-Turner <joel.hammond-turner@landmark.co.uk>
2019-03-19 14:07:17 +0000

    Added sample log and renamed stuff

--- Snippets/WhenTheWorldFallsDown/OurFault-2.cs
+++ Snippets/WhenTheWorldFallsDown/OurFault-2.cs
@@ -1,9 +1,5 @@
 var responses = new List<string>();
 var docData = new Dictionary<string, List<Document>>();
-var surveyorSysrefs = new List<string>();
-
-Log.WriteEntry("About to check {0} cases for surveyor {1}", true, cases.Length, surveyorCode);
-
 using (var database = new Database())
 {
     if (database.OpenDB("Case") == 0)
@@ -19,7 +15,6 @@ using (var database = new Database())
             {
                 // This sysref doesnt exist at all!
                 responses.Add(string.Format("{0}:D", sysref));
-                Log.WriteEntry("Case {0} does not exist.", true, sysref);
                 continue;
             }
 
@@ -28,25 +23,15 @@ using (var database = new Database())
             if (database.DBFld[survcodeFld] != surveyorCode)
             {
                 responses.Add(string.Format("{0}:R:{1}", sysref, database.DBFld[survcodeFld]));
-                Log.WriteEntry("Case {0} is not allocated to surveyor {1}.", true, sysref, surveyorCode);
                 continue;
             }
-
-            surveyorSysrefs.Add(sysref);
+            else
+            {
+                // Include document data if necessary.
+                if (includeDocumentData)
+                {
+                    docData.Add(sysref, new List<Document>(Document.GetDocuments(Program.DbName, sysref)));
+                }
+            }
         }
     }
-}
-                
-Log.WriteEntry(
-    "About to get documents for {0} cases allocated to surveyor {1}", 
-    true,
-    surveyorSysrefs.Count, 
-    surveyorCode);
-
-var allDocs = Document.GetDocuments(Program.DbName, surveyorSysrefs.ToArray());
-
-foreach (var sysRef in surveyorSysrefs)
-{
-    var docs = allDocs.Where(d => d.SysRef == sysRef).ToList();
-    docData.Add(sysRef, docs);
-}
--- Snippets/WhenTheWorldFallsDown/OurFault-3.cs
+++ Snippets/WhenTheWorldFallsDown/OurFault-3.cs
@@ -1,5 +1,9 @@
 var responses = new List<string>();
 var docData = new Dictionary<string, List<Document>>();
+var surveyorSysrefs = new List<string>();
+
+Log.WriteEntry("About to check {0} cases for surveyor {1}", true, cases.Length, surveyorCode);
+
 using (var database = new Database())
 {
     if (database.OpenDB("Case") == 0)
@@ -15,6 +19,7 @@ using (var database = new Database())
             {
                 // This sysref doesnt exist at all!
                 responses.Add(string.Format("{0}:D", sysref));
+                Log.WriteEntry("Case {0} does not exist.", true, sysref);
                 continue;
             }
 
@@ -23,15 +28,25 @@ using (var database = new Database())
             if (database.DBFld[survcodeFld] != surveyorCode)
             {
                 responses.Add(string.Format("{0}:R:{1}", sysref, database.DBFld[survcodeFld]));
+                Log.WriteEntry("Case {0} is not allocated to surveyor {1}.", true, sysref, surveyorCode);
                 continue;
             }
-            else
-            {
-                // Include document data if necessary.
-                if (includeDocumentData)
-                {
-                    docData.Add(sysref, new List<Document>(Document.GetDocuments(Program.DbName, sysref)));
-                }
-            }
+
+            surveyorSysrefs.Add(sysref);
         }
     }
+}
+                
+Log.WriteEntry(
+    "About to get documents for {0} cases allocated to surveyor {1}", 
+    true,
+    surveyorSysrefs.Count, 
+    surveyorCode);
+
+var allDocs = Document.GetDocuments(Program.DbName, surveyorSysrefs.ToArray());
+
+foreach (var sysRef in surveyorSysrefs)
+{
+    var docs = allDocs.Where(d => d.SysRef == sysRef).ToList();
+    docData.Add(sysRef, docs);
+}
